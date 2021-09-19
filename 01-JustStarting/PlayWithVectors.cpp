988de816aeb36633bcaa5a941c552531e1f04acf
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2021-09-19 17:50:59 +0100

    feat: JustStarting - add some vectors playing

--- 01-JustStarting/PlayWithVectors.cpp
+++ 01-JustStarting/PlayWithVectors.cpp
@@ -5,20 +5,35 @@ using std::endl;
 
 #include <string>
 using std::string;
-
 #include "PlayWithStrings.h"
+
+#include <vector>
+using std::vector;
 
 //
 // Constants
 //
 
 // 
-// Common generation of vectors
+// Common operations
 //
+string getSentence(vector<string> words)
+{
+    string sentence = "";
+    for (int wordPosition = 0; wordPosition < words.size(); wordPosition++)
+    {
+        sentence = sentence + " " + words[wordPosition];
+    }
+    return sentence;
+}
 
 // 
 // Common formatted string writing
 //
+void writeSentence(vector<string> words)
+{
+    cout << getSentence(words) << endl;
+}
 
 // 
 // Main method
@@ -27,14 +42,20 @@ void playWithVectors()
 {
     writeHeader("PLAYING WITH VECTORS!!!");
 
+    vector<string> words;
     string someInput = "";
     while (someInput != "bye")
     {
         writeLine("[write 'bye' to exit]");
         writeSeparator();
 
-        cout << "TODO... ask input for some vector game: ";
+        cout << "Write a word: ";
         cin >> someInput;
+        writeSeparator();
+
+        writeLine("Current sentence so far:");
+        words.push_back(someInput);
+        writeSentence(words);
         writeSeparator();
     }
 
