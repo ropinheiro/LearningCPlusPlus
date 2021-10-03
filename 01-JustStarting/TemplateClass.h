cb690fde5a011082b598e79511628b938c08fe8b
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2021-10-03 11:24:02 +0100

    feat: JustStarting - add playing with specalization of a template class

--- 01-JustStarting/TemplateClass.h
+++ 01-JustStarting/TemplateClass.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+using std::string;
+
 template <class T>
 class TemplateClass
 {
@@ -9,5 +12,21 @@ public:
     T GetMin(T const& t1, T const& t2)
     {
         return t1 < t2 ? t1 : t2;
+    }
+};
+
+// This is a specialization for bool values
+template <>
+class TemplateClass<bool>
+{
+private:
+
+public:
+    // Instead of returning a bool value (that gets written as 0 or 1
+    // when converting to string) return hardcoded strings T/F.
+    string GetMin(bool const& t1, bool const& t2)
+    {
+        bool result = t1 < t2 ? t1 : t2;
+        return result ? "T" : "F";
     }
 };
